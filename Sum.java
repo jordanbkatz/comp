9c4aab0ef2b883a7d7e4ba37b00a062d535722c5
Jordan <jordanbkatz4@gmail.com>
2022-02-11 20:50:26 -0500

    added sum

---
+++ Sum.java
@@ -0,0 +1,35 @@
+import java.util.*;
+//import java.io.*;
+public class Sum {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        try {
+            int t = s.nextInt();
+            s.nextLine();
+            int n = s.nextInt();
+            s.nextLine();
+            int[][] in = new int[t][n];
+            int[] out = new int[t];
+            for (int i = 0; i < in.length; i++) {
+                for (int j = 0; j < n; j++) {
+                    in[i][j] = s.nextInt();
+                    s.nextLine();
+                }
+                out[i] = sum(in[i]);
+            }
+            for (int j = 0; j < out.length; j++) {
+                System.out.println(out[j]);
+            }
+        }
+        finally {
+            s.close();
+        }
+    }
+    private static int sum(int[] in) {
+        int out = 0;
+        for (int i = 0; i < in.length; i++) {
+            out += in[i];
+        }
+        return out;
+    }
+}
