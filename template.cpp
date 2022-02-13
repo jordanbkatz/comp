cc1954aecbb499dbb7d683f90c6e8e44b8984423
Jordan <jordanbkatz4@gmail.com>
2022-02-13 13:12:20 -0500

    migrated template from java to c++

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+void solve(int tc) {
+    cout << tc << endl;
+}
+int main() {
+    int tc = 1;
+    cin >> tc;
+    while (tc > 0) {
+        solve(tc);
+        tc--;
+    }
+    return 0;
+}
