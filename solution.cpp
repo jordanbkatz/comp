1977788b7a57c1cf63c6268d7648d4b1454faf58
Jordan <jordanbkatz4@gmail.com>
2022-04-01 14:41:19 -0400

    added empty readme and updated template

--- solution.cpp
+++ solution.cpp
@@ -1,27 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+typedef long long int ll;
 typedef long double ld;
+#define mod (int) 1e9 + 7
 #define ln '\n'
 #define vt vector
 #define pb push_back
 #define mp make_pair
 #define ff first
 #define ss second
-#define all(x) x.begin(), x.end()
 #define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#define dbg(x) cout << #x << " => "; _P(x); cout << ln
-template <typename T> void _P(T &);
-template <typename T, typename V> void _P(pair<T, V> &);
-template <typename T> void _P(vector<T> &);
-template <typename T> void _P(set<T> &);
-template <typename T, typename V> void _P(map<T, V> &);
-template <typename T> void _P(T &x) {cout << x << " ";}
-template <typename T, typename V> void _P(pair<T, V> &p) {cout << "( "; _P(p.ff); _P(p.ss); cout << ") ";}
-template <typename T> void _P(vector<T> &v) {cout << "[ "; for (T &x : v) _P(x); cout << "] ";}
-template <typename T> void _P(set<T> &s) {cout << "{ "; for (const T &x : s) _P(x); cout << "} ";}
-template <typename T, typename V> void _P(map<T, V> &m) {cout << "{ "; for (auto x: m) _P(x); cout << "} ";}
-const int MOD = (int) 1e9 + 7;
+#define all(x) (x).begin(), (x).end()
+#define dbg(x) cout << #x << " => " << x << ln
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) {
+    return os << "( " << p.ff << " " << p.ss << " )";
+}
+template<typename C, typename T = typename enable_if<!is_same<C, string>::value, typename C::value_type>::type>
+ostream &operator<<(ostream &os, const C &c) {
+    os << "[ "; for (const T &x : c) os << x << " "; return os << "] ";
+}
 namespace solution {
     void solve() {
         
