1e64edb593e486cad623c9a0932adae2818e2e1c
Jordan Katz <jordankatz@Jordans-MBP.localdomain>
2022-04-16 19:47:27 -0400

    added ld and ull typedefs

--- sol.cpp
+++ sol.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
 #define vt vector
 #define pb push_back
 #define mp make_pair
